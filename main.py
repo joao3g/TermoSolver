6883986141d5191e4d28a82e3a7f8e6d03d893ce
joao3g <joao.almeida.gabriel2015@gmail.com>
2022-09-27 23:53:54 -0300

    Solved two entrys the same letter in a word and now ignore blacks letters when the same letter yellow letter is seted.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ excluded = []
 contains = {}
 awnser = {0: '', 1: '', 2: '', 3: '', 4: ''}
 exclusiveOne = []
+TwoOrMore = []
 
 def checkExcluded(word):
      for letter in excluded:
@@ -20,8 +21,12 @@ def checkContains(word):
      for letter in contains:
           if(word.find(letter) == -1):
                return False
+
+          index = word.find(letter)
                
           occurrences = [m.start() for m in re.finditer(letter, word)]
+
+
           for i in occurrences:
                for j in contains[letter]:
                     if(j == i):
@@ -36,6 +41,12 @@ def checkExact(word):
                     return False
      return True
 
+def checkTwoMore(word):
+     for letter in TwoOrMore:
+          if(word.count(letter) < 2):
+               return False
+     return True
+
 def checkExclusivelyOne(word):
      for letter in exclusiveOne:
           if(word.count(letter) != 1):
@@ -43,6 +54,8 @@ def checkExclusivelyOne(word):
      return True
 
 def getInfo():
+     word = {}
+
      data = input("Insira os dados descobertos (Letra, Posicao) (0 para letra invalida): ")
      if data==0:
           exit()
@@ -54,12 +67,17 @@ def getInfo():
           
           letter = infoArray[0]
           position = int(infoArray[1])
+          word[position] = letter
 
           # Impede de remover letra já inserida
           for index in awnser:
                if(awnser[index]==letter and position==0):
                     do = False
                     exclusiveOne.append(letter)
+          
+          for index in contains:
+               if(index==letter and position==0):
+                    do = False
           
           if(do):
                if(position > 0):
@@ -72,13 +90,20 @@ def getInfo():
                else:
                     excluded.append(letter)
 
+     for position in word:
+          letter = word[position]
+          if(position < 0):
+               for i in word:
+                    if(letter==word[i] and i>0):
+                         TwoOrMore.append(letter)
+
 def mainLoop():
 
      while True:
           getInfo()
 
           for word in words:
-               if(checkExcluded(word) and checkContains(word) and checkExact(word) and checkExclusivelyOne(word)):
+               if(checkExcluded(word) and checkContains(word) and checkExact(word) and checkExclusivelyOne(word) and checkTwoMore(word)):
                     print(word)
 
 mainLoop()
